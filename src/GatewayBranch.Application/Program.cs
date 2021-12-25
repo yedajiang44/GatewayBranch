5f15efa25fc7cc6a9f73c674b5d5bcea78d43aa1
yedajiang44 <602830483@qq.com>
2021-12-25 19:24:31 +0800

    update net6

--- src/GatewayBranch.Application/Program.cs
+++ src/GatewayBranch.Application/Program.cs
@@ -3,21 +3,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
-using System.Threading.Tasks;
 
-namespace GatewayBranch.Application
+await Host.CreateDefaultBuilder(args)
+.ConfigureServices((hostContext, services) =>
 {
-    class Program
-    {
-        static Task Main(string[] args) => Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services
-                .AddGatewayBranch(hostContext.Configuration)
-                .AddLogging(logger =>
-                {
-                    logger.ClearProviders().AddNLog(new NLogLoggingConfiguration(hostContext.Configuration.GetSection("NLog")));
-                });
-            }).RunConsoleAsync();
-    }
-}
+    services
+    .AddGatewayBranch(hostContext.Configuration)
+    .AddLogging(logger => logger.ClearProviders().AddNLog(new NLogLoggingConfiguration(hostContext.Configuration.GetSection("NLog"))));
+}).RunConsoleAsync();
